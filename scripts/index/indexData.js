5829e585915a698f2403ba20cce663b50cf9fbd6
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-04-23 11:20:47 -0600

    Create project

---
+++ scripts/index/indexData.js
@@ -0,0 +1,27 @@
+"use strict";
+
+let newsArray = [
+  {
+    title: "EFT VS Wire Transfers: What's the Difference?",
+    timestamp: "2022/04/18",
+    author: "Ryleigh Stangness",
+    thumbnail: "./../../assets/images/news/20220418-post.jpeg",
+    description:
+      "EFT (PAD) or classic wire transfer?” And which one should you choose for your business or personal banking needs? Read on to find out more about these two payment types.",
+  },
+  {
+    title: "What You Need To Know About The New April 2022 Interchange Rates",
+    timestamp: "2022/04/12",
+    author: "Ryleigh Stangness",
+    thumbnail: "./../../assets/images/news/20220412-post.jpeg",
+    description:
+      "Let's look at the increases, decreases and new interchange rates this April 2022 to see how merchants will be impacted and where they can save.",
+  },
+  {
+    title: "High-Risk Merchants: Find The Processor for You",
+    author: "Ryleigh Stangness",
+    thumbnail: "./../../assets/images/news/20220407-post.jpeg",
+    description:
+      "High risk merchant? Here is how to find the right processor. Plus, tips to get approved for a merchant account sooner.",
+  },
+];
