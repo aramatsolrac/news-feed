9aa04f64abac8774941c0bcf02e5a77c915e5d7c
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-04-23 19:15:32 -0600

    Update utils.js

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,1 +1,19 @@
 "use strict";
+//utility to build DOM elements with classes and text content
+function createElement(element, className = null, text = null) {
+  const newElement = document.createElement(element);
+  if (text) newElement.textContent = text;
+  if (className) newElement.classList.add(className);
+  return newElement;
+}
+
+function formatDate(date) {
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "2-digit",
+    timeZone: "UTC",
+  };
+
+  return new Date(Number(date)).toLocaleDateString("en-US", options);
+}
