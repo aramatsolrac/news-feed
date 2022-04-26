f24fdb1ba07f7f94000365c24b285e02ab12f2f2
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-04-26 11:40:12 -0600

    Remove title and add likes

--- scripts/index/indexData.js
+++ scripts/index/indexData.js
@@ -2,27 +2,36 @@
 
 let newsArray = [
   {
-    title: "EFT VS Wire Transfers: What's the Difference?",
     timestamp: "1649986800000",
+    author: "Helcim Team",
+    thumbnail: "",
+    description:
+      "Did you know we recently won Canada's Most Admired Company Culture award with @waterstonehc?  But what makes for an amazing #companyculture? Check out our video below and find out! http://ow.ly/fo4r50IKg3u",
+    likes: 104,
+  },
+
+  {
+    timestamp: "1649976800000",
     author: "Ryleigh Stangness",
     thumbnail: "./../../assets/images/news/20220418-post.jpeg",
     description:
       "EFT (PAD) or classic wire transfer? And which one should you choose for your business or personal banking needs? Read on to find out more about these two payment types.",
+    likes: 28,
   },
   {
-    title: "What You Need To Know About The New April 2022 Interchange Rates",
     timestamp: "1649725600000",
     author: "Ryleigh Stangness",
     thumbnail: "./../../assets/images/news/20220412-post.gif",
     description:
       "Let's look at the increases, decreases and new interchange rates this April 2022 to see how merchants will be impacted and where they can save.",
+    likes: 203,
   },
   {
-    title: "High-Risk Merchants: Find The Processor for You",
     timestamp: "1649293600000",
     author: "Ryleigh Stangness",
     thumbnail: "./../../assets/images/news/20220407-post.jpeg",
     description:
       "High risk merchant? Here is how to find the right processor. Plus, tips to get approved for a merchant account sooner.",
+    likes: 601,
   },
 ];
